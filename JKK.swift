7c7aa6481c98a7efea90ddf5fe375fc2b7e64760
lewisfeng <>
2022-08-24 16:47:26 -0700

    3

--- JKK.swift
+++ JKK.swift
@@ -8,23 +8,3 @@
 
 import UIKit
 
-public class JKLabel : UILabel {
-
-    /**
-        Tells the label to start blinking.
-    */
-    public func startBlinking() {
-        let options : UIViewAnimationOptions = .repeat
-      UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
-            self.alpha = 0
-            }, completion: nil)
-    }
-
-    /**
-    Tells the label to stop blinking.
-    */
-    public func stopBlinking() {
-        alpha = 1
-        layer.removeAllAnimations()
-    }
-}
--- lew999.swift
+++ lew999.swift
@@ -7,4 +7,24 @@
 
 import Foundation
 
-// thisi
+public class JKLabel : UILabel {
+
+    /**
+        Tells the label to start blinking.
+    */
+    public func startBlinking() {
+        let options : UIViewAnimationOptions = .repeat
+      UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
+            self.alpha = 0
+            }, completion: nil)
+    }
+
+    /**
+    Tells the label to stop blinking.
+    */
+    public func stopBlinking() {
+        alpha = 1
+        layer.removeAllAnimations()
+    }
+}
+
